f14e719e2b24be380c60dcf84a6c9b86409c4a1d
sanjana k <sanjanarthi@gmail.com>
2021-03-21 10:49:28 +0530

    "Added UC1 - Day 14"

--- UserRegistrationDay14/Program.cs
+++ UserRegistrationDay14/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace UserRegistrationDay14
 {
-    class Program
+    public class Program
     {
+        public bool validateName(string name)
+        {
+            string pattern = @"^[A-Z]\w{2,}$";
+            bool result = Regex.IsMatch(name, pattern);
+            if (!result)
+            {
+                throw new UserException("Name Validation Failed");
+            }
+            return result;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration Problem");
+            Console.WriteLine("Welcome to unit testing of user registration");
+            bool result = false; ;
+
+            Program program = new Program();
+            try
+            {
+                result = program.validateName("Sanjana");
+            }
+            catch (UserException)
+            {
+                Console.WriteLine("Exception caught");
+            }
+
+            Console.WriteLine("First name validation result: " + result);
         }
     }
 }
